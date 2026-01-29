df13b52e3ea4b8b4f8f6d4479af0d618d6691f94
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-01-29 23:32:07 +0530

    Productivity Dashboard

---
+++ script.js
@@ -0,0 +1,8 @@
+var allElems = document.querySelectorAll(".elem");
+var allFullElems = document.querySelectorAll(".fullElem");
+allElems.forEach(function (elem) {
+  elem.addEventListener("click", function () {
+    // console.log(elem.id);
+    allFullElems[elem.id].style.display = "block";
+  });
+});
